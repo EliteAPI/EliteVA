0f8f0705d16e2eabd6c5550db814b7dc67984a04
Somfic <somfic@gmail.com>
2021-09-19 22:59:45 +0200

    Fixed logging

--- EliteVA/VoiceAttackPlugin.cs
+++ EliteVA/VoiceAttackPlugin.cs
@@ -24,22 +24,22 @@ namespace EliteVA
 
         private static WebSocket _socket;
         private static VoiceAttackProxy _proxy;
-
-        private static int _maxReconnectionAttempts = 3;
-        private static int _reconnectionAttempts = 0;
-
+        
+        private static bool _hasConnected = false;
         private static bool _hasCaughtUp = false;
 
         public static void VA_Init1(dynamic vaProxy)
         {
             _proxy = new VoiceAttackProxy(vaProxy);
-            _socket = new WebSocket("ws://localhost:8001/ws", "EliteAPI-plugin");
+            _socket = new WebSocket("ws://localhost:51555/ws", "EliteAPI-plugin");
 
             _socket.Opened += SocketOpened;
             _socket.Error += SocketError;
             _socket.Closed += SocketClosed;
             _socket.MessageReceived += SocketMessage;
 
+            _hasConnected = false;
+            Log("Connecting to EliteAPI Hub ...", VoiceAttackColor.Gray);
             _socket.Open();
         }
 
@@ -50,32 +50,28 @@ namespace EliteVA
 
         private static void SocketClosed(object sender, EventArgs e)
         {
-            Log("The connection to EliteAPI Hub has closed", VoiceAttackColor.Red);
-
-            if (_reconnectionAttempts == _maxReconnectionAttempts)
+            if (_hasConnected)
             {
-                Log(
-                    "Could not establish a connection to EliteAPI Hub. EliteAPI will not be functional. Restart VoiceAttack to try again",
-                    VoiceAttackColor.Purple);
-                return;
+                Log("The connection to EliteAPI Hub was closed", VoiceAttackColor.Red);
+                Log($"Trying to reconnect to EliteAPI Hub ... ", VoiceAttackColor.Gray);
             }
 
-            _reconnectionAttempts++;
-            Thread.Sleep(3000);
-            Log($"Trying to reconnect to EliteAPI Hub ({_reconnectionAttempts}/{_maxReconnectionAttempts}) ... ",
-                VoiceAttackColor.Gray);
+            _hasConnected = false;
             _socket.Open();
         }
 
         private static void SocketError(object sender, ErrorEventArgs e)
         {
-            Log($"Could not connect to EliteAPI Hub ({e.Exception.GetType().Name}: {e.Exception})",
-                VoiceAttackColor.Purple);
+            if (_hasConnected)
+            {
+                Log($"Could not connect to EliteAPI Hub ({e.Exception.GetType().Name}: {e.Exception})",
+                    VoiceAttackColor.Purple);
+            }
         }
 
         private static void SocketOpened(object sender, EventArgs e)
         {
-            _reconnectionAttempts = 0;
+            _hasConnected = true;
             Log("Authenticating with EliteAPI Hub", VoiceAttackColor.Gray);
             SendMessage(new WebSocketMessage("auth", "plugin"));
         }
@@ -142,7 +138,7 @@ namespace EliteVA
                         eventValue.Variables.ForEach(SetVariable);
                         if (_hasCaughtUp)
                         {
-                            Log($"Invoking ((EliteAPI.{eventValue.Name}))", VoiceAttackColor.Pink);
+                            //Log($"Invoking ((EliteAPI.{eventValue.Name}))", VoiceAttackColor.Pink);
                             _proxy.Commands.Invoke($"((EliteAPI.{eventValue.Name}))");
                         }
                     }
@@ -188,8 +184,8 @@ namespace EliteVA
                         _proxy.Variables.Set(variable.Name, bool.Parse(variable.Value.ToString()));
                         break;
                 }
-                
-                Log($"Set {variable.Name} to {variable.Value} ({variable.Type})", VoiceAttackColor.Pink);
+
+                //Log($"Set {variable.Name} to {variable.Value} ({variable.Type})", VoiceAttackColor.Pink);
             }
             catch (Exception ex)
             {
